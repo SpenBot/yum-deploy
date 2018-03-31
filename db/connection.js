8204b228dff85e5681d3ddfb9cd65b7a7854c8ed
Spenser Holstein <spenser.holstein@gmail.com>
2018-03-31 15:32:42 -0400

    again, more styling and fixed connection logs

--- db/connection.js
+++ db/connection.js
@@ -18,11 +18,11 @@ mongoose.Promise = Promise
 if (process.env.NODE_ENV == "production") {
 
   mongoose.connect(process.env.MLAB_URL)
-    .catch(err => console.log(`\n\tConnection failed : ${err}`))
     .then(connection => {
       console.log(`\n\tConnection established to PRODUCTION database.`)
       // console.log(`\n\tConnection established to PRODUCTION DB : '${process.env.MLAB_URL}'\n`)
     })
+    .catch(err => console.log(`\n\tConnection failed : ${err}`))
 
 // if other environments
 } else {
@@ -30,10 +30,10 @@ if (process.env.NODE_ENV == "production") {
   const mongoUri = 'mongodb://localhost/review-yum-db'
 
   mongoose.connect(mongoUri)
-    .catch(err => console.log(`\n\tConnection failed : ${err}`))
     .then(connection => {
       console.log(`\n\tConnection established to LOCAL database : '${mongoUri}'\n`)
     })
+    .catch(err => console.log(`\n\tConnection failed : ${err}`))
 
 }
 
